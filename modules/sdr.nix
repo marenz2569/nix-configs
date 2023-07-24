6e1ba79d56e9692a8c73206afb711cd421b0cbc1
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-07-24 12:04:04 +0200

    add custom soapysdr build

--- modules/sdr.nix
+++ modules/sdr.nix
@@ -2,6 +2,32 @@
   hardware.hackrf.enable = true;
   hardware.rtl-sdr.enable = true;
 
-  environment.systemPackages = with pkgs; [ gqrx inspectrum sigdigger baudline cubicsdr limesuite ];
+  services.sdrplayApi.enable = true;
+  nixpkgs.overlays = [
+    (self: super: {
+      soapysdr-with-plugins = super.soapysdr-with-plugins.override {
+        extraPackages = with super; [
+          limesuite
+          soapyairspy
+          soapyaudio
+          soapybladerf
+          soapyhackrf
+          soapyremote
+          soapyrtlsdr
+          soapyuhd
+          soapysdrplay
+        ];
+      };
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [
+    gqrx
+    inspectrum
+    sigdigger
+    baudline
+    cubicsdr
+    limesuite
+  ];
   services.udev.packages = with pkgs; [ limesuite ];
 }
